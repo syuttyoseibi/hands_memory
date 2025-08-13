14e41a30c6362d55b1d197bdecc371fcc2a7934c
syuttyoseibi <daishi2matsuda@gmail.com>
2025-08-13 22:53:49 +0900

    アフィリエイト追加

--- app/page.js
+++ app/page.js
@@ -92,6 +92,23 @@ export default function HomePage() {
           </div>
         )}
       </div>
+
+      {/* Ad Block 1 */}
+      <div className="text-center mt-5 mb-4">
+        <div dangerouslySetInnerHTML={{ __html: `<a href="https://px.a8.net/svt/ejp?a8mat=45BSYY+2P1P5M+4PWE+BYLJL" rel="nofollow">
+          <img border="0" width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=250813546163&wid=002&eno=01&mid=s00000022019002009000&mc=1"></a>
+          <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=45BSYY+2P1P5M+4PWE+BYLJL" alt="">
+        ` }} />
+      </div>
+
+      {/* Ad Block 2 */}
+      <div className="text-center mb-5">
+        <div dangerouslySetInnerHTML={{ __html: `<a href="https://px.a8.net/svt/ejp?a8mat=45BSYY+3PYLFM+2PEO+C2O5D" rel="nofollow">
+          <img border="0" width="468" height="60" alt="" src="https://www28.a8.net/svt/bgt?aid=250813546225&wid=003&eno=01&mid=s00000012624002028000&mc=1"></a>
+          <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=45BSYY+3PYLFM+2PEO+C2O5D" alt="">
+        ` }} />
+      </div>
+
     </div>
   );
 }
